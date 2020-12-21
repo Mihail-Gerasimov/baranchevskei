10d78cbc6ea2def4a12a5c73baccc07b60aec763
Михаил <65629866+Mihail-Gerasimov@users.noreply.github.com>
2020-12-21 18:27:50 +0400

    'f'

---
+++ js/main.js
@@ -0,0 +1,10 @@
+  $(document).ready(function() {
+      // slider
+      $('.fade').slick({
+          dots: true,
+          infinite: true,
+          speed: 500,
+          fade: true,
+          cssEase: 'linear'
+      });
+  });
